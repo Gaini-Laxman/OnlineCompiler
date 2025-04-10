1d7012828f0a7d2cc6140900343f620ee0a0cb0f
Gaini Laxman <gainilaxman12@gmail.com>
2025-04-10 10:22:21 +0530

    changes added

--- client/src/FeatureView.jsx
+++ client/src/FeatureView.jsx
@@ -1,27 +1,29 @@
-import Editor from '@monaco-editor/react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function FeatureView(props) {
-  const [get, SetGet] = useState("")
-  const params = useParams()
+  const [get, SetGet] = useState("");
+  const params = useParams();
   useEffect(() => {
     async function getCode() {
-      const resp = await fetch(`http://localhost:8000/code/${params.codeid}`)
-      const code = await resp.json()
-      SetGet(code[0])
+      const resp = await fetch(
+        `https://backendserver-production-8815.up.railway.app/code/${params.codeid}`
+      );
+      const code = await resp.json();
+      SetGet(code[0]);
     }
-    getCode()
-  }, [params.codeid])
+    getCode();
+  }, [params.codeid]);
   return (
     <Editor
       fontSize="22px"
       height="94vh"
       width="100%"
-      theme={props.dark ? 'vs-dark' : "vs"}
-      language={get.language==='py'?"python":get.language}
+      theme={props.dark ? "vs-dark" : "vs"}
+      language={get.language === "py" ? "python" : get.language}
       value={get.code}
     />
-  )
+  );
 }
--- client/src/View.jsx
+++ client/src/View.jsx
@@ -1,25 +1,27 @@
-import Editor from '@monaco-editor/react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function View(props) {
-  const [get, SetGet] = useState("")
+  const [get, SetGet] = useState("");
   useEffect(() => {
     async function getCode() {
-      const resp = await fetch(`http://localhost:8000/code/${props.codeid}`)
-      const code = await resp.json()
-      SetGet(code[0])
+      const resp = await fetch(
+        `https://backendserver-production-8815.up.railway.app/code/${props.codeid}`
+      );
+      const code = await resp.json();
+      SetGet(code[0]);
     }
-    getCode()
-  }, [props.codeid])
+    getCode();
+  }, [props.codeid]);
   return (
     <Editor
       fontSize="22px"
       height="94vh"
       width="100%"
-      theme={props.dark ? 'vs-dark' : "vs"}
-      language={get.language==='py'?"python":get.language}
+      theme={props.dark ? "vs-dark" : "vs"}
+      language={get.language === "py" ? "python" : get.language}
       value={get.code}
     />
-  )
+  );
 }
